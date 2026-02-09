023f9a770d1b6e9a25e64b5619b212858ffd4c27
Azaz Khan <azazkhan.it07@gmail.com>
2026-02-09 17:03:45 +0530

    feat(transaction): add transaction entity and repository layer

---
+++ payflow-backend/src/main/java/com/novapay/payflow_backend/transaction/entity/Transaction.java
@@ -0,0 +1,41 @@
+package com.novapay.payflow_backend.transaction.entity;
+
+import com.novapay.payflow_backend.transaction.entity.enums.TransactionStatus;
+import com.novapay.payflow_backend.transaction.entity.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String referenceId;
+    @Column(name = "sender_wallet_id")
+    private Long senderWalletId;
+    @Column(name = "receiver_wallet_id")
+    private Long receiverWalletId;
+    @Column(name = "amount")
+    private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
